d002052c468755cbe6f54a0e32aea0a545e71a08
alexandrecpedro <alexandrecpedro@gmail.com>
2022-03-07 20:26:11 -0300

    AboutMe app

---
+++ README.md
@@ -0,0 +1,20 @@
+## AboutMe
+
+The AboutMe app is a demo app that shows information about a person.
+* Name
+* Settable Nickname
+* An image
+* Scrollable information
+
+This app demonstrates the following views and techniques:
+* LinearLayout
+* TextView
+* EditText
+* ImageView
+* ScrollView
+* Setting multiple click handlers programmatically
+* Using data binding for views to improve performance and eliminate findViewById
+
+## Screenshots
+
+![Screenshot1](screenshots/AboutMeScreenshots.png)
---
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.aboutme
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
+
+/* AboutMe app. This app demonstrates:
+*     * LinearLayout with TextViews, ImageView, Button, EditText, and ScrollView
+*     * ScrollView to display scrollable text
+*     * Getting user input with an EditText.
+*     * Click handler for a Button to retrieve text from an EditText and set it in a TextView.
+*     * Setting the visibility status of a view.
+*     * Data binding between MainActivity and activity_main.xml. How to remove findViewById,
+*       and how to display data in views using the data binding object.
+*/
+class MainActivity : AppCompatActivity() {
+
+    // Binding object for MainActivity.
+    // Name of the object is derived from the name of the activity or fragment.
+    private lateinit var binding: ActivityMainBinding
+
+    // Instance of MyName data class.
+    private val myName: MyName = MyName("Alexandre Pedro")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Setting the content view using DataBindingUtil creates an instance of
+        // ActivityMainBinding from the supplied activity and the supplied layout. This object
+        // contains mappings between the activity and layout,
+        // and functionality to interact with them.
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        // Set the value of the myName variable that is declared and used in the layout file.
+        binding.myName = myName
+
+        // Click listener for the Done button.
+        binding.doneButton.setOnClickListener {
+            // it = Done button (view: View)
+            addNickname(it)
+        }
+    }
+
+    /*
+       * Click handler for the Done button.
+       * Demonstrates how data binding can be used to make code much more readable
+       * by eliminating calls to findViewById and changing data in the binding object.
+    */
+    private fun addNickname(view: View) {
+        binding.apply {
+            // Set the text for nicknameText to the value in nicknameEdit.
+            myName?.nickname = nicknameEdit.text.toString()
+            // Invalidate all binding expressions and request a new rebind to refresh UI
+            invalidateAll()
+            // Change which views are visible.
+            // Remove the EditText and the Button.
+            // With GONE they are invisible and do not occupy space.
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+
+            // Make the TextView with the nickname visible.
+            nicknameText.visibility = View.VISIBLE
+        }
+        // Hide the keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
